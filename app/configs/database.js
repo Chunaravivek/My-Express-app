42f3d3f3334db18a9fa46bc60843fe17809a8f5d
vivek.chunara <vivek.chunara@iflair.com>
2023-08-03 18:58:30 +0530

    database issue fixed

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 module.exports = function (logger) {
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(
-        () => logger.info(`Connected to development DB...`)
-    ).catch((error) => logger.info(`MongoDB connection error: ${error}`));
+	mongoose.connect(process.env.DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).then(() => {
+		logger.info(`Connected to development DB...`)
+	}).catch((error) => {
+		logger.info(`MongoDB connection error: ${error}`)
+	});
 }
