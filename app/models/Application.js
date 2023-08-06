6001121e424716aadb526806ec8953d47bbf78dd
Chunaravivek <chunaravivek6351@gmail.com>
2023-08-06 22:18:31 +0530

    change files

--- app/models/Application.js
+++ app/models/Application.js
@@ -1,82 +1,82 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Applicationchema = 
-new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        maxlength: 255,
+const Applicationchema =
+    new mongoose.Schema({
+        name: {
+            type: String,
+            trim: true,
+            maxlength: 255,
+        },
+        app_version: {
+            type: String,
+            trim: true,
+            maxlength: 255,
+            default: "1.0",
+        },
+        account_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Account',
+            autopopulate: { select: 'name' },
+        },
+        package_name: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 255,
+        },
+        firebase_id: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 255,
+        },
+        sender_id: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 255,
+        },
+        app_code: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 255,
+        },
+        account_url: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 255,
+        },
+        recmnd_status: {
+            type: String,
+            enum: ['1', '2'],
+            default: "1",
+        },
+        ternding_status: {
+            type: String,
+            enum: ['1', '2'],
+            default: "2",
+        },
+        play_store: {
+            type: String,
+            enum: ['1', '2', '3'],
+            default: "1",
+        },
+        status: {
+            type: Boolean,
+            default: true,
+            index: true
+        }
     },
-    app_version: {
-        type: String,
-        trim: true,
-        maxlength: 255,
-        default: "1.0",
-    },
-    account_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Account',
-        autopopulate: {select: 'name'},
-    },
-    package_name: {
-        type: String,
-        trim: true,
-        minlength: 3,
-        maxlength: 255,
-    },
-    firebase_id: {
-        type: String,
-        trim: true,
-        minlength: 3,
-        maxlength: 255,
-    },
-    sender_id: {
-        type: String,
-        trim: true,
-        minlength: 3,
-        maxlength: 255,
-    },
-    app_code: {
-        type: String,
-        trim: true,
-        minlength: 3,
-        maxlength: 255,
-    },
-    account_url: {
-        type: String,
-        trim: true,
-        minlength: 3,
-        maxlength: 255,
-    },
-    recmnd_status: {
-        type: String,
-        enum: ['1', '2'],
-        default: "1",
-    },
-    ternding_status: {
-        type: String,
-        enum: ['1', '2'],
-        default: "2",
-    },
-    play_store: {
-        type: String,
-        enum: ['1', '2', '3'],
-        default: "1",
-    },
-    status: {
-        type: Boolean,
-        default: true,
-        index: true
-    }
-},
-{ timestamps: true });
+        { timestamps: true });
 
 const Application = mongoose.model('Application', Applicationchema);
 
 function ValidateApplication(application) {
     const schema = Joi.object({
-        id : Joi.string(),
+        id: Joi.string(),
         name: Joi.string().min(3).max(255).required(),
         app_version: Joi.string().min(3).max(255).required(),
         package_name: Joi.string().min(3).max(255).required(),
@@ -85,7 +85,7 @@ function ValidateApplication(application) {
         account_id: Joi.string().min(3).max(255).required(),
         account_url: Joi.string().min(3).max(255).required(),
     });
-    
+
     return schema.validate(application);
 }
 
